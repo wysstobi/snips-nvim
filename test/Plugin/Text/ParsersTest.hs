08a6f0a830b508fce337d4195b33c4b3eb2b90fa
Andri Wild <andri.wild@students.fhnw.ch>
2023-05-24 20:19:08 +0200

    add parseUntil tests

--- test/Plugin/Text/ParsersTest.hs
+++ test/Plugin/Text/ParsersTest.hs
@@ -1,12 +1,46 @@
 module Plugin.Text.ParsersTest where
 
+import Data.List
 import Test.Tasty
 import Test.Tasty.HUnit
-import Plugin.Text.Text (getReplacementForKey)
+import Plugin.Text.Parsers
+import Test.Tasty.QuickCheck
 
 parsersTests :: TestTree
-parsersTests = testGroup "parsers" [testEmptyReplacement]
+parsersTests = testGroup "parsers" [
+  testSimpleParseUntil, 
+  testQuoteSignBeforeQuoteParseUntil, 
+  testPrefixParseUntil,
+  testSuffixParseUntil,
+  testPreAndSuffixParseUntil
+  ]
 
-testEmptyReplacement :: TestTree
-testEmptyReplacement = testCase "test getReplacementForKey2" $
-  getReplacementForKey [] "" @?= ""
+quote :: String
+quote = "<>"
+
+testSimpleParseUntil :: TestTree
+testSimpleParseUntil = testCase "test parseUntil simple case" $
+  parse (parseUntil quote) "123<>abc" @?= Just ("123","abc")
+
+testQuoteSignBeforeQuoteParseUntil :: TestTree
+testQuoteSignBeforeQuoteParseUntil = testCase "test parseUntil with char from quote before quote" $
+  parse (parseUntil quote) ("123" ++ take 1 quote ++ quote ++ "abc") @?= Just ("123" ++ take 1 quote,"abc")
+
+testPrefixParseUntil :: TestTree
+testPrefixParseUntil  = testProperty "test parseUntil with arbitrary prefix" $
+   \x ->
+     not (quote `isInfixOf` (x :: String)) ==>
+      parse (parseUntil quote) (x ++ quote) == Just (x, "")
+
+testSuffixParseUntil :: TestTree
+testSuffixParseUntil = testProperty "test parseUntil with arbitrary suffix" $
+   \x ->
+     not (quote `isInfixOf` (x :: String)) ==>
+      parse (parseUntil quote) (quote ++ x) == Just ("", x)
+
+testPreAndSuffixParseUntil :: TestTree
+testPreAndSuffixParseUntil = testProperty "test parseUntil with arbitrary pre and suffix" $
+   \x y ->
+     not (quote `isInfixOf` (x :: String)) ==>
+     not (quote `isInfixOf` (y :: String)) ==>
+      parse (parseUntil quote) (x ++ quote ++ y) == Just (x, y)
