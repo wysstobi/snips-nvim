04c6a79719d90cf62e4b5506f43245481992840a
Andri Wild <andri.wild@students.fhnw.ch>
2023-06-01 09:39:22 +0200

    add test cases to Text file

--- src/Plugin/Types.hs
+++ src/Plugin/Types.hs
@@ -6,19 +6,25 @@ import Neovim
 data SnipsEnv = SnipsEnv { names :: String, snippetPath :: String, qs :: Quotes }
 
 newtype SnippetMetaData = SnippetMetaData { fileType :: String } deriving Show
+instance Eq SnippetMetaData where
+  (SnippetMetaData ft1) == (SnippetMetaData ft2) = ft1 == ft2
 
 type SnipsNvim a = Neovim SnipsEnv a
 data Snippet = Snippet { name :: String, content :: [String], meta :: SnippetMetaData } deriving Show
 
+instance Eq Snippet where
+  (Snippet n1 c1 m1) == (Snippet n2 c2 m2) = n1 == n2 && c1 == c2  && m1 == m2
+
 type Quotes = (String, String)
 -- State
-data Placeholder = Placeholder { key :: String, value :: Maybe String } deriving Show
+data Placeholder = Placeholder { key :: String, value :: Maybe String } deriving (Show, Eq)
 
-data PlaceholderState = PS { snippet :: Snippet, quotes :: Quotes, placeholders :: [Placeholder] } deriving Show
+-- instance Eq Placeholder where
+--   (Placeholder k v) == (Placeholder k2 v2) = k == k2 && v == v2
+
+data PlaceholderState = PS { snippet :: Snippet, quotes :: Quotes, placeholders :: [Placeholder] } deriving (Show, Eq)
 
 
 type PlaceholderST a = StateT PlaceholderState (Neovim SnipsEnv) a
-
-
 
 
