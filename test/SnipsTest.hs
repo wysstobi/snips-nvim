fd43e93b406e6ee3de03b8764a68bcffe77e9ddd
Andri Wild <andri.wild@students.fhnw.ch>
2023-06-03 21:29:40 +0200

    remove unused imports and files

--- test/SnipsTest.hs
+++ test/SnipsTest.hs
@@ -7,26 +7,10 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
 import Data.List
-import Plugin.Text.Text (getReplacementForKey)
-
-import Plugin.Text.TextTest(textTests)
-import Plugin.Text.ParsersTest(parsersTests)
+import Plugin.Text.SearchingTest (searchingTests)
+import Plugin.Text.ReplacingTest (replacingTests)
 
 main =  defaultMain $ testGroup "snips tests" [
-   textTests, parsersTests
+   searchingTests, replacingTests 
    ]
 
-
-
-
--- Task 1.1
--- ========
--- 
--- Verify that sorting a list does not change it's length
---
--- Note that we have to use a type annotation to force the type of the list elements. This is necessary
--- as QuickCheck can only generate random values of a concrete type, but not for an unknown type variable.
-testPreservesLength :: TestTree
-testPreservesLength = testProperty "Sort preserves length" $
-  -- placeholder - modify as necessary
-  \xs -> (length $ sort (xs :: [Int])) == length xs
