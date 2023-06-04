ab22cbf9c764661264b8aef32f76a098e91f0ae5
Andri Wild <andri.wild@students.fhnw.ch>
2023-06-04 18:25:16 +0200

    update preview to show snippet content

--- lua/telescope-integration.lua
+++ lua/telescope-integration.lua
@@ -7,18 +7,6 @@ local action_state = require "telescope.actions.state"
 local previewers = require "telescope.previewers"
 
 
-function MyFunction (tobi)
-   print("helo telenames" .. tobi)
-   return "helo from MyFunction (telenames)"
-end
-
---local s = vim.api.nvim_command_output('echo FprodTeam()')
-
--- t={}
--- for i in string.gmatch(s, "%P+") do
--- 		table.insert(t,i)
--- end
-
 local attach_mappings = function (prompt_bufnr, map)
 		actions.select_default:replace(function ()
 				actions.close(prompt_bufnr)
@@ -42,34 +30,68 @@ local marker = function(entry)
 		}
 end
 
--- local previewer = Previewer:new(opts)
-
-
--- local previewer = previewers.new_termopen_previewer()
--- previewer.get_command = function (entry, status)
---         return {'bat', entry.path}
--- end
-
-
---
---print(vim.inspect(t))
-function run(table)
-        local opts = {
-                prompt_title = "best guys ever",
-                layout_strategy = "horizontal",
-                layout_config = { height = 0.95 },
-                finder = finders.new_table(table),
-                sorter = conf.generic_sorter(),
-                attach_mappings = attach_mappings,
-                entry_maker = marker,
-                previewer = previewers.new_buffer_previewer {
-                      title = "My preview",
-                      define_preview = function (self, entry, status)
-                              vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, {entry[1], "print(\"Hello World\")"})
-                      end
-                }}
-        local names = pickers.new(opts)
-        names:find()
+local function printTable( t )
+ 
+    local printTable_cache = {}
+ 
+    local function sub_printTable( t, indent )
+ 
+        if ( printTable_cache[tostring(t)] ) then
+            print( indent .. "*" .. tostring(t) )
+        else
+            printTable_cache[tostring(t)] = true
+            if ( type( t ) == "table" ) then
+                for pos,val in pairs( t ) do
+                    if ( type(val) == "table" ) then
+                        print( indent .. "[" .. pos .. "] => " .. tostring( t ).. " {" )
+                        sub_printTable( val, indent .. string.rep( " ", string.len(pos)+8 ) )
+                        print( indent .. string.rep( " ", string.len(pos)+6 ) .. "}" )
+                    elseif ( type(val) == "string" ) then
+                        print( indent .. "[" .. pos .. '] => "' .. val .. '"' )
+                    else
+                        print( indent .. "[" .. pos .. "] => " .. tostring(val) )
+                    end
+                end
+            else
+                print( indent..tostring(t) )
+            end
+        end
+    end
+ 
+    if ( type(t) == "table" ) then
+        print( tostring(t) .. " {" )
+        sub_printTable( t, "  " )
+        print( "}" )
+    else
+        sub_printTable( t, "  " )
+    end
 end
 
+local function getKeysFromTable(snippetTable)
+local keyTable = {}
+  for key,value in pairs(snippetTable) do
+    table.insert(keyTable, key)
+    print(key)
+  end
+  return keyTable
+end
 
+function run(table)
+       local keys = getKeysFromTable(table)
+       local opts = {
+               prompt_title = "best guys ever",
+               layout_strategy = "horizontal",
+               layout_config = { height = 0.95 },
+               finder = finders.new_table(keys),
+               sorter = conf.generic_sorter(),
+               attach_mappings = attach_mappings,
+               entry_maker = marker,
+               previewer = previewers.new_buffer_previewer{
+                     title = "My preview",
+                     define_preview = function (self, entry, status)
+                             vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, table[entry[1]])
+                     end
+               }}
+       local names = pickers.new(opts)
+       names:find()
+end
