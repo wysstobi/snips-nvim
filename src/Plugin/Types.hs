e8e30667aad7ce340c7652214b934ec0aed19faf
Raphael Lüthy <raphael.luethy@students.fhnw.ch>
2023-06-05 13:50:28 +0200

    📝 docs(Types.hs): add Aeson instances and documentation for Snippet, SnippetMetaData, and SnippetList types to improve code readability and maintainability

--- src/Plugin/Types.hs
+++ src/Plugin/Types.hs
@@ -28,12 +28,28 @@ data PlaceholderState = PS { snippet :: Snippet, quotes :: Quotes, placeholders 
 
 type PlaceholderST a = StateT PlaceholderState (Neovim SnipsEnv) a
 
--- JSON
+
+-- | Aeson instance for snippet metadata
+-- Used to create a JSON object from snippet metadata
 instance FromJSON SnippetMetaData where
   parseJSON = withObject "SnippetMetaData" $ \v -> do
     fileTypes <- v .: "fileTypes"
     return $ SnippetMetaData fileTypes
 
+{- | Aeson instance for SnippetMetaData
+Used to create a JSON object from SnippetMetaData
+
+__Example:__
+@
+meta : {
+  "fileTypes": [
+    "haskell",
+    "python"
+  ]
+}
+@
+
+-}
 instance ToJSON SnippetMetaData where
   toJSON (SnippetMetaData fileTypes) = object ["fileTypes" .= fileTypes]
 
@@ -44,15 +60,40 @@ instance FromJSON Snippet where
     meta <- v .: "meta"
     return $ Snippet name content meta
 
+
+{- | Aeson instance for Snippet
+Used to create a JSON object from a snippet
+
+__Example:__
+
+@
+{
+  "name": "Best Snippet Ever",
+  "content": [
+    "main = putStrLn \"Hello, World!\"\n"
+  ],
+  "meta": {...}
+}
+@
+-}
 instance ToJSON Snippet where
   toJSON (Snippet name content meta) = object ["name" .= name, "content" .= content, "meta" .= meta]
 
+-- | Type used to represent a list of snippets
 newtype SnippetList = SnippetList {snippets :: [Snippet]} deriving (Show)
 
+-- | Aeson instance for SnippetList
+-- Used to create a list of snippets from a JSON object
 instance FromJSON SnippetList where
+  -- | Parse a JSON object into a SnippetList
   parseJSON = withObject "SnippetList" $ \v -> do
+    -- | Get the list of snippets from the JSON object
     snippets' <- v .: "snippets"
+    -- | Return a SnippetList
     return $ SnippetList snippets'
 
+-- | Aeson instance for SnippetList
+-- Used to create a JSON object from a list of snippets
 instance ToJSON SnippetList where
+  -- | Create a JSON object from a SnippetList
   toJSON (SnippetList snippets') = object ["snippets" .= snippets']
